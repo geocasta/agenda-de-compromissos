af67743893e261bb74d49bc28e4ce946ce74b233
geocasta souza <geocastasouza9@gmail.com>
2024-04-09 10:44:44 -0300

    limpei o modelo

--- main.c
+++ main.c
@@ -1,27 +1,4 @@
-//  a agenda contara com um sistem simples de cadastro de compromissos e prioridades do dia
-//  a a
-//Agenda deve conter as seguintes informações.
-//MENUS
-//1 - Cadastrar compromisso
-//  nome do compromisso
-//  data do compromisso
-//  hora do compromisso  
-//  prioridade do compromisso
-//  a agenda deve conter as seguintes funções.
-//  2- Listar compromisso
-//  listar compromissos por prioridade
-//  listar compromissos por data
-//  listar compromissos por hora
-//  listar compromissos por nome
-//  MENUS 
-//  3- Editar compromisso 
-//  visualizar compromisso
-//  alterar data 
-//  alterar hora
-//  alterar prioridade
-//  remover
-// 4- Sair
-//  sair do programa
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -309,34 +286,3 @@ void tela_excluir_compromi(void) {
 
 
 
-/*void menu_principal(agenda) {
-    int opcao;
-    do {
-        printf("\nMENU PRINCIPAL\n");
-        printf("1 - Cadastrar compromisso\n");
-        printf("2 - Listar compromissos\n");
-        printf("3 - Editar compromisso\n");
-        printf("4 - Sair\n");
-        printf("Escolha a opção: ");
-        scanf("%d", &opcao);
-
-        switch (opcao) {
-            case 1:
-                cadastrar_compromisso();
-                break;
-            case 2:
-                listar_compromisso();
-              
-                break;
-            case 3:
-                editar_compromisso();
-                break;
-            case 4:
-                printf("Saindo do programa...\n");
-                break;
-            default:
-                printf("Opção inválida. Por favor, escolha uma opção válida.\n");
-        }
-    } while (opcao != 4);
-  }
-*/
