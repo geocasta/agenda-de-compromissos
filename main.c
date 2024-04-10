ff7d0ce98d634ad26889429058b637e2bb844deb
geocasta souza <geocastasouza9@gmail.com>
2024-04-09 23:20:22 -0300

    modulando

--- main.c
+++ main.c
@@ -12,18 +12,6 @@
 
 
 
-
-//
-// Assinatura das funções
-void tela_menu_principal(void);
-void tela_sobre(void);
-void tela_equipe(void);
-void tela_menu_compromi(void);
-void tela_cadastrar_compromi(void);
-void tela_pesquisar_compromi(void);
-void tela_alterar_compromi(void);
-void tela_excluir_compromi(void);
-
 //
 // Programa principal
 int main(void) {
