e602e568a28dd5a4fdcf856ca16c8296618c1683
geocasta souza <geocastasouza9@gmail.com>
2024-04-16 20:26:01 -0300

    sobre pondo h

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "agenda.h"
+#include "sobre.h"
 
 // ###             Universidade Federal do Rio Grande do Norte                ###
 // ###                 Centro de Ensino Superior do Seridó                    ###
