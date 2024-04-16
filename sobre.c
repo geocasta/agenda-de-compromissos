768f834ae46c36a6f2df7f1ba4905f71a544987c
geocasta souza <geocastasouza9@gmail.com>
2024-04-16 20:29:07 -0300

    teladobre

---
+++ sobre.c
@@ -0,0 +1,27 @@
+void tela_sobre(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("##############################################################################\n");
+    printf("###                                                                        ###\n");
+    printf("###             Universidade Federal do Rio Grande do Norte                ###\n");
+    printf("###                 Centro de Ensino Superior do Seridó                    ###\n");
+    printf("###               Departamento de Computação e Tecnologia                  ###\n");
+    printf("###                  Disciplina DCT1106 -- Programação                     ###\n");
+    printf("###               Projeto Agenda de compromissos                           ###\n");
+    printf("###               Developed by @Geocastasouza                              ###\n");
+    printf("###                                                                        ###\n");
+    printf("##############################################################################\n");
+    printf("###                                                                        ###\n");
+    printf("###                = = = = Agenda compromisso = = = = =                    ###\n");
+    printf("###                                                                        ###\n");
+    printf("### Programa exemplo utilizado na disciplina DCT1106 - Programação, para   ###\n");
+    printf("### fins didáticos de ilustração. O programa contém os principais módulos  ###\n");
+    printf("### e funcionalidades que serão exigidos na avaliação dos projetos a serem ###\n");
+    printf("### desenvolvidos,nesse por exemplo sera uma agenda de compromissos evetos ###\n");
+    printf("### pra melhor organização dos eventos do dia-dia.                         ###\n");
+    printf("###                                                                        ###\n");
+    printf("##############################################################################\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
